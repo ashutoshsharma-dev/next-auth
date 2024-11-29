1ab47fbfdddd2b2c79dfdb20b0535989da70e4ef
Ashutosh Sharma <ashutosh.squadifypro007@gmail.com>
2024-11-29 11:03:36 +0530

    feat: add github login

---
+++ src/lib/authOptions.js
@@ -0,0 +1,14 @@
+import GithubProvider from 'next-auth/providers/github';
+
+export const authOptions = {
+    // Secret for Next-auth, without this JWT encryption/decryption won't work
+    secret: process.env.NEXTAUTH_SECRET,
+
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
+    ],
+};
