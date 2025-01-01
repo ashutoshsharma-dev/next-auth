f1eafa0f8b4527a97af4e84d707d128ab322bfce
Ashutosh Sharma <ashutosh.squadifypro007@gmail.com>
2025-01-01 11:39:50 +0530

    feat: add chatbot

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+   <head>
+
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/6774dba4af5bfec1dbe569ea/1igg7juq2';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+  </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
